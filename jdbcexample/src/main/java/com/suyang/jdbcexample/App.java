f635dd9d75d1df5b6740dbc3b6e8665fddaa809f
xsi640 <xsi640@hotmail.com>
2017-06-07 14:16:53 +0800

    init code

---
+++ jdbcexample/src/main/java/com/suyang/jdbcexample/App.java
@@ -0,0 +1,13 @@
+package com.suyang.jdbcexample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
