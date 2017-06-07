e2ba4b83096aef016b364e6b57c026aa9d20bd5d
xsi640 <xsi640@hotmail.com>
2017-06-07 15:26:40 +0800

    jdbc example 完成。

---
+++ jdbcexample/src/main/java/com/suyang/dao/StudentDao.java
@@ -0,0 +1,17 @@
+package com.suyang.dao;
+
+import java.util.List;
+
+import com.suyang.domain.Student;
+
+public interface StudentDao {
+	Student get(int id);
+	
+	Student insert(Student s);
+
+	Student update(Student s);
+
+	int delete(int id);
+
+	List<Student> selectAll();
+}
---
+++ jdbcexample/src/main/java/com/suyang/dao/utils/DBUtils.java
@@ -0,0 +1,26 @@
+package com.suyang.dao.utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBUtils {
+
+	private static final String driver = "com.mysql.jdbc.Driver";
+	private static final String url = "jdbc:mysql://localhost:3306/test";
+	private static final String username = "root";
+	private static final String password = "123123";
+
+	public static Connection getConnection() {
+		Connection conn = null;
+		try {
+			Class.forName(driver);
+			conn = (Connection) DriverManager.getConnection(url, username, password);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return conn;
+	}
+}
