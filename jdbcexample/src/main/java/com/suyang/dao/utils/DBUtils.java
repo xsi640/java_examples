10fc712455e5f0c44198e85cd2af22ee04db613d
xsi640 <xsi640@hotmail.com>
2017-06-11 19:19:02 +0800

    完成。

--- jdbcexample/src/main/java/com/suyang/dao/utils/DBUtils.java
+++ jdbcexample/src/main/java/com/suyang/dao/utils/DBUtils.java
@@ -9,7 +9,7 @@ public class DBUtils {
 	private static final String driver = "com.mysql.jdbc.Driver";
 	private static final String url = "jdbc:mysql://localhost:3306/test";
 	private static final String username = "root";
-	private static final String password = "123123";
+	private static final String password = "830128";
 
 	public static Connection getConnection() {
 		Connection conn = null;
--- jpa_example/src/main/java/com/suyang/domain/Student.java
+++ jpa_example/src/main/java/com/suyang/domain/Student.java
@@ -6,12 +6,9 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
-import javax.persistence.Table;
-
-import org.springframework.data.annotation.Id;
+import javax.persistence.Id;
 
 @Entity
-@Table(name = "student")
 public class Student {
 
 	@Id
