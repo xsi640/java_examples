95b3933446ae0b0ca6da34467f5b72685f83fa2a
xsi640 <xsi640@hotmail.com>
2017-06-20 22:58:34 +0800

    Springboot uploadfile

---
+++ springboot_uploadfile/src/main/java/com/suyang/Application.java
@@ -0,0 +1,27 @@
+package com.suyang;
+
+import javax.servlet.MultipartConfigElement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+	@Bean 
+    public MultipartConfigElement multipartConfigElement() { 
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        //// 设置文件大小限制 ,超了，页面会抛出异常信息，这时候就需要进行异常信息的处理了;
+        factory.setMaxFileSize("128MB"); //KB,MB
+        /// 设置总上传数据总大小
+        factory.setMaxRequestSize("512MB"); 
+        //Sets the directory location where files will be stored.
+        //factory.setLocation("路径地址");
+        return factory.createMultipartConfig(); 
+    } 
+}
