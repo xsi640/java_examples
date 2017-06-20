3e65c843334c92fc8a40a48f6d8fa0144a4382e2
xsi640 <xsi640@hotmail.com>
2017-06-20 16:58:43 +0800

    springboot jsp

---
+++ springboot_jsp/src/main/java/com/suyang/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.suyang.controller;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+	// 从 application.properties 中读取配置，如取不到默认值为Hello jsp
+	@Value("${application.hello:Hello Angel}")
+	private String hello;
+
+	@RequestMapping("/helloJsp")
+	public String helloJsp(Map<String, Object> map) {
+		System.out.println("HelloController.helloJsp().hello=" + hello);
+		map.put("hello", hello);
+		return "helloJsp";
+	}
+}
