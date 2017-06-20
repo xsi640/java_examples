9ba9ffb38a35432fc011baf935bdb4a669a6b0fe
xsi640 <xsi640@hotmail.com>
2017-06-20 17:12:11 +0800

    springboot servlet

---
+++ springboot_servlet/src/main/java/com/suyang/servlet/MyServlet.java
@@ -0,0 +1,41 @@
+package com.suyang.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 这种方法配合Application类中的getMyServlet()注入的Bean使用
+ * @author xsi64
+ *
+ */
+public class MyServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(">>>>>>>>>>doGet()<<<<<<<<<<<");
+		doPost(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println(">>>>>>>>>>doPost()<<<<<<<<<<<");
+		resp.setContentType("text/html");
+		resp.setCharacterEncoding("UTF-8");
+		PrintWriter out = resp.getWriter();
+		out.println("<html>");
+		out.println("<head>");
+		out.println("<title>Hello World</title>");
+		out.println("</head>");
+		out.println("<body>");
+		out.println("<h1>这是：MyServlet</h1>");
+		out.println("</body>");
+		out.println("</html>");
+	}
+}
