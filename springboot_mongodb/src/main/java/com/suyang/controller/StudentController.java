becb50209e92426f41e65f3f401fa8e78a7e63ee
xsi640 <xsi640@hotmail.com>
2017-06-23 15:51:46 +0800

    springboot mongodb

---
+++ springboot_mongodb/src/main/java/com/suyang/controller/StudentController.java
@@ -0,0 +1,54 @@
+package com.suyang.controller;
+
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.suyang.domain.Student;
+import com.suyang.repository.StudentRepository;
+
+@RestController
+public class StudentController {
+
+	@Autowired
+	private StudentRepository studentRepository;
+
+	@RequestMapping(value = "/student/{id}", method = RequestMethod.GET)
+	public Student findOne(@PathVariable("id") final String id) {
+		return studentRepository.findOne(id);
+	}
+
+	@RequestMapping(value = "/student", method = RequestMethod.GET)
+	public List<Student> findAll() {
+		return studentRepository.findAll();
+	}
+
+	@RequestMapping(value = "/student", method = RequestMethod.POST)
+	public Student create(String id, String name, Integer age, Date birthday) {
+		Student student = new Student();
+		student.setId(id);
+		student.setName(name);
+		student.setAge(age);
+		student.setBirthday(birthday);
+		return studentRepository.save(student);
+	}
+
+	@RequestMapping(value = "/student", method = RequestMethod.PUT)
+	public Student modify(String id, String name, Integer age, Date birthday) {
+		Student student = studentRepository.findOne(id); 
+		student.setName(name);
+		student.setAge(age);
+		student.setBirthday(birthday);
+		return studentRepository.save(student);
+	}
+
+	@RequestMapping(value = "/student/{id}", method = RequestMethod.DELETE)
+	public void delete(@PathVariable("id") String id) {
+		studentRepository.delete(id);
+	}
+}
